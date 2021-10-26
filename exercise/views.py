8262d9ff48dcd58b4837c1090c5d07887e24b0a9
정순호 <aschung@jeongsunhoui-MacBookAir.local>
2021-10-26 18:23:23 +0900

    Added first working post function

--- exercise/views.py
+++ exercise/views.py
@@ -5,6 +5,7 @@ from rest_framework.decorators import api_view
 import datetime
 import json
 from .models import *
+from users.models import User
 from .serializers import ExerciseDetailsSerializer, ExerciseSerializer, TodayExerciseTimeSerializer, UserExercisePlanWeightSerializer,  UserExercisePlanWeightSetsSerializer, UserExerciseRecordWeightSerializer, UserExerciseRecordWeightSetsSerializer
 # Create your views here.
 
@@ -33,60 +34,68 @@ def getTodayExerciseTime(request):
     serializer = TodayExerciseTimeSerializer(duration)
     return Response(serializer.data)
 
+
 @api_view(['GET'])
 def getExerciseDetails(request):
     exercise_id = request.GET['exercise_id']
-    details = ExerciseDetails.objects.filter(exercise = exercise_id)
+    details = ExerciseDetails.objects.filter(exercise=exercise_id)
     serializer = ExerciseDetailsSerializer(details)
     return Response(serializer.data)
+
 
 @api_view(['GET'])
 def getUserExercisePlanWeight(request):
     userId = request.GET('user_id')
     exerciseId = request.GET('exercise_id')
-    plan = ExercisePlanWeight.objects.filter(user = userId, exercise = exerciseId)
-    serializer = UserExercisePlanWeightSerializer(plan, many = True)
+    plan = ExercisePlanWeight.objects.filter(user=userId, exercise=exerciseId)
+    serializer = UserExercisePlanWeightSerializer(plan, many=True)
     return Response(serializer.data)
+
 
 @api_view(['GET'])
 def getUserExercisePlanWeightSets(request):
     planId = request.GET('exercise_plan_weight_id')
-    plan_sets = ExercisePlanWeightSet.objects.filter(exercise_plan_weight = planId)
-    serializer = UserExercisePlanWeightSetsSerializer(plan_sets, many = True)
+    plan_sets = ExercisePlanWeightSet.objects.filter(
+        exercise_plan_weight=planId)
+    serializer = UserExercisePlanWeightSetsSerializer(plan_sets, many=True)
     return Response(serializer.data)
+
 
 @api_view(['GET'])
 def getUserAllExerciseRecordWeight(request):
     userId = request.GET('user_id')
-    record = ExerciseRecordWeight.objects.filter(user = userId)
-    serializer = UserExerciseRecordWeightSerializer(record, many = True)
+    record = ExerciseRecordWeight.objects.filter(user=userId)
+    serializer = UserExerciseRecordWeightSerializer(record, many=True)
     return Response(serializer.data)
+
 
 @api_view(['GET'])
 def getUserExerciseRecordWeight(request):
     userId = request.GET('user_id')
     date = request.GET('date')
     parsedDate = datetime.datetime.strptime(date, '%m/%d/%Y')
-    record = ExerciseRecordWeight(user = userId, date = parsedDate)
-    serializer = UserExerciseRecordWeightSerializer(record, many = True)
+    record = ExerciseRecordWeight(user=userId, date=parsedDate)
+    serializer = UserExerciseRecordWeightSerializer(record, many=True)
     return Response(serializer.data)
+
 
 @api_view(['GET'])
 def getUserExerciseRecordWeightSets(request):
     record_id = request.GET('exercise_record_weight_id')
-    record_sets = ExerciseRecordWeightSet.objects.filter(exercise_record_weight = record_id)
-    serializer = UserExerciseRecordWeightSetsSerializer(record_sets, many = True)
+    record_sets = ExerciseRecordWeightSet.objects.filter(
+        exercise_record_weight=record_id)
+    serializer = UserExerciseRecordWeightSetsSerializer(record_sets, many=True)
     return Response(serializer.data)
+
 
 @api_view(['POST'])
 def postUserExercisePlanWeight(request):
     request = json.loads(request.body)
-    plan = ExercisePlanWeight(user = request['user'], exercise = request['exercise'], date = request['date'], num_sets = request['num_sets'])
+    plan = ExercisePlanWeight(user_id=request['user'], exercise_id=request['exercise'], date=request['date'], num_sets=request['num_sets'])
     plan.save()
-
+    
     sets = request['sets']
-    print(sets)
-    #for set in sets:
-    #    data = ExercisePlanWeightSet(exercise_plan_weight = set[''], set_num = set['set_num'], target_weight = set['target_weight'], target_reps = set['target_reps'])
-    #    data.save()
-    return HttpResponse(status = 200)
+    for set in sets:
+       data = ExercisePlanWeightSet(exercise_plan_weight_id = plan.id, set_num = set['set_num'], target_weight = set['target_weight'], target_reps = set['target_reps'])
+       data.save()
+    return HttpResponse(status=200)
