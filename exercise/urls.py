2f3cfe8604a9f221564cfe6764c26dfa90dfb9b8
YooSang Choo <rogerchoo@snu.ac.kr>
2021-11-04 16:26:15 +0900

    post record 함수 추가

--- exercise/urls.py
+++ exercise/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import getExercise, getExerciseDetails, getExercisePlanWeight,  getTodayExerciseTime, getUserAllExerciseRecordWeight, getUserExercisePlanWeight, getUserExercisePlanWeightSets, getUserExerciseRecordWeight, getUserExerciseRecordWeightSets, postUserExercisePlanWeight
+from .views import getExercise, getExerciseDetails, getExercisePlanWeight,  getTodayExerciseTime, getUserAllExerciseRecordWeight, getUserExercisePlanWeight, getUserExercisePlanWeightSets, getUserExerciseRecordWeight, getUserExerciseRecordWeightSets, postUserExercisePlanWeight, postUserExerciseRecordWeight
 
 
 urlpatterns = [
@@ -11,6 +11,7 @@ urlpatterns = [
     path('record_weight', getUserAllExerciseRecordWeight), #user_id
     path('record_weight_date', getUserExerciseRecordWeight), #user_id & date
     path('record_weight_sets', getUserExerciseRecordWeightSets), #exercise_record_weight_id
-    path('post_exercise_plan_weight', postUserExercisePlanWeight),
-    path('exercise_plan_weight', getExercisePlanWeight), #exercise_plan_weight_id & set_num
+    path('post_exercise_plan_weight', postUserExercisePlanWeight), #user_id & exercise_id & date & num_sets & sets
+    path('exercise_plan_weight', getExercisePlanWeight), #exercise_plan_weight_id
+    path('post_exercise_record_weight', postUserExerciseRecordWeight),
 ]
