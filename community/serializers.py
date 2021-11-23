4a699180f5c70beb2f232cdba5da0778ee986965
YooSang Choo <rogerchoo@snu.ac.kr>
2021-11-23 19:21:53 +0900

    getPost, getQna 수정 postPost, postQna 추가

--- community/serializers.py
+++ community/serializers.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.db.models import fields
 from rest_framework import serializers
 
-from community.models import Post, PostPreview, QnaPost, QnaPreview
+from community.models import Post, PostComment, PostCommentReply, PostPreview, Qna, QnaAnswer, QnaAnswerComment, QnaAnswerCommentReply, QnaPost, QnaPostAnswer, QnaPreview
 
 class PostSerializer(serializers.ModelSerializer):
 
@@ -10,20 +10,54 @@ class PostSerializer(serializers.ModelSerializer):
         model = Post
         fields = ('user', 'creation_date', 'title', 'content', 'modified')
 
-class PostPreviewSerializer(serializers.ModelSerializer):
+class PostCommentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = PostComment
+        fields = ('user', 'content', 'creation_date')
+        
+class PostCommentReplySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = PostCommentReply
+        fields = ('user', 'post_comment','content', 'creation_date')
+
+class PostCommentReplyFinalSerializer(serializers.Serializer):
+    post = PostSerializer()
+    comment = PostCommentSerializer(many = True)
+    reply = PostCommentReplySerializer(many = True)
     
+class PostPreviewSerializer(serializers.ModelSerializer):
     class Meta:
         model = PostPreview
         fields = ('user', 'title', 'creation_date')
 
 class QnaSerializer(serializers.ModelSerializer):
-
     class Meta:
-        model = QnaPost
+        model = Qna
         fields = ('user', 'title', 'content', 'creation_date', 'modified')
 
-class QnaPreviewSerializer(serializers.ModelSerializer):
+class QnaAnswerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = QnaAnswer
+        fields = ('user', 'content', 'creation_date')
 
+class QnaAnswerFinalSerializer(serializers.Serializer):
+    qna = QnaSerializer()
+    answer = QnaAnswerSerializer(many = True)
+    
+class QnaAnswerCommentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = QnaAnswerComment
+        fields = ('user', 'content', 'creation_date')
+
+class QnaAnswerCommentReplySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = QnaAnswerCommentReply
+        fields = ('user', 'qna_answer_comment', 'content', 'creation_date')
+        
+class QnaAnswerCommentReplyFinalSerializer(serializers.Serializer):
+    comment = QnaAnswerCommentSerializer(many = True)
+    reply = QnaAnswerCommentReplySerializer(many = True)
+class QnaPreviewSerializer(serializers.ModelSerializer):
     class Meta:
         model = QnaPreview
         fields = ('user', 'title', 'creation_date', 'solved')
