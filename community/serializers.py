f88284fc6c35fb40b85d179e7aa529df2869e510
YooSang Choo <rogerchoo@snu.ac.kr>
2021-12-01 19:38:50 +0900

    get 함수 수정

--- community/serializers.py
+++ community/serializers.py
@@ -1,52 +1,60 @@
+from re import T
 from django.db import models
 from django.db.models import fields
+from django.utils import tree
 from rest_framework import serializers
 
-from community.models import Post, PostComment, PostCommentReply, PostCount, Qna, QnaAnswer, QnaAnswerComment, QnaAnswerCommentReply, QnaPreview
+from community.models import Post, PostComment, PostCommentReply, PostCount, Qna, QnaAnswer, QnaAnswerComment, QnaAnswerCommentReply, QnaCount, QnaPreview
 
 class PostSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Post
-        fields = ('user', 'creation_date', 'title', 'content', 'modified')
+        fields = "__all__"
 
 class PostCommentSerializer(serializers.ModelSerializer):
     class Meta:
         model = PostComment
-        fields = ('user', 'content', 'creation_date')
+        fields = "__all__"
         
 class PostCommentReplySerializer(serializers.ModelSerializer):
     class Meta:
         model = PostCommentReply
-        fields = ('user', 'post_comment','content', 'creation_date')
+        fields = "__all__"
 
 class PostCommentReplyFinalSerializer(serializers.Serializer):
-    post = PostSerializer()
+    post = PostSerializer(many = True)
     comment = PostCommentSerializer(many = True)
     reply = PostCommentReplySerializer(many = True)
     
 class PostCountSerializer(serializers.ModelSerializer):
     class Meta:
         model = PostCount
-        fields = ('count_likes', 'count_comments', 'count_saved')
+        fields = "__all__"
+
 class PostSimpleSerializer(serializers.ModelSerializer):
     class Meta:
         model = Post
-        fields = ('user', 'title', 'creation_date')
+        fields = "__all__"
 
 class PostPreviewSerializer(serializers.Serializer):
-    post = PostSimpleSerializer
-    count = PostCountSerializer
+    post = PostSerializer(many = True)
+    count = PostCountSerializer(many = True)
 
 class QnaSerializer(serializers.ModelSerializer):
     class Meta:
         model = Qna
-        fields = ('user', 'title', 'content', 'creation_date', 'modified')
+        fields = "__all__"
 
 class QnaAnswerSerializer(serializers.ModelSerializer):
     class Meta:
         model = QnaAnswer
-        fields = ('user', 'content', 'creation_date')
+        fields = "__all__"
+
+class QnaCountSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = QnaCount
+        fields = "__all__"
 
 class QnaAnswerFinalSerializer(serializers.Serializer):
     qna = QnaSerializer()
@@ -55,17 +63,17 @@ class QnaAnswerFinalSerializer(serializers.Serializer):
 class QnaAnswerCommentSerializer(serializers.ModelSerializer):
     class Meta:
         model = QnaAnswerComment
-        fields = ('user', 'content', 'creation_date')
+        fields = "__all__"
 
 class QnaAnswerCommentReplySerializer(serializers.ModelSerializer):
     class Meta:
         model = QnaAnswerCommentReply
-        fields = ('user', 'qna_answer_comment', 'content', 'creation_date')
+        fields = "__all__"
         
 class QnaAnswerCommentReplyFinalSerializer(serializers.Serializer):
     comment = QnaAnswerCommentSerializer(many = True)
     reply = QnaAnswerCommentReplySerializer(many = True)
-class QnaPreviewSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = QnaPreview
-        fields = ('user', 'title', 'creation_date', 'solved')
+    
+class QnaPreviewSerializer(serializers.Serializer):
+    qna = QnaSerializer(many = True)
+    count = QnaCountSerializer(many = True)
