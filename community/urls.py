8574cae6580af85494c59bae8d32caf4c5cc867a
YooSang Choo <rogerchoo@snu.ac.kr>
2021-12-02 19:35:20 +0900

    수정

--- community/urls.py
+++ community/urls.py
@@ -1,20 +1,25 @@
 from django.urls import path
 
-from community.views import QnaAnswerCommentReplyViewset,  QnaAnswerViewset, getHotBoardPreview, getPostCommentReply, getPostPreview, getQnaMain, getQnaMainSolved, getQnaMainType, getQnaMainUnsolved, postPost, postQna
+from community.views import getHotBoardPreview, getPostCommentReply, getPostPreview, getQnaAnswer, getQnaAnswerCommentReply, getQnaMain, getQnaMainSolved, getQnaMainType, getQnaMainUnsolved, postPost, postPostComment, postPostCommentReply, postQna, postQnaAnswer, postQnaAnswerComment, postQnaAnswerCommentReply
 
 urlpatterns = [
     ########게시판 메인화면###########
     path('postmain', getPostPreview), #page_num
     path('hotboardmain', getHotBoardPreview), #page_num
     path('qnamain', getQnaMain), #page_num
-    path('QnaMainSolved', getQnaMainSolved), #page_num
-    path('QnaMainUnsolved', getQnaMainUnsolved), #page_num
-    path('QnaMainType', getQnaMainType), #page_num & type
+    path('qnamainsolved', getQnaMainSolved), #page_num
+    path('qnamainunsolved', getQnaMainUnsolved), #page_num
+    path('qnamaintype', getQnaMainType), #page_num & type
     ########게시물 확인###############
     path('getpost', getPostCommentReply), #post_id
-    path('QnaAnswer', QnaAnswerViewset.list), #qna_id
-    path('QnaAnswerCommentReply', QnaAnswerCommentReplyViewset.list), #qna_answer_id   
+    path('qnaanswer', getQnaAnswer), #qna_id
+    path('qnaanswercommentreply', getQnaAnswerCommentReply), #qna_answer_id   
     ########게시물 작성###############
-    path('Post', postPost),
-    path('Qna', postQna),
+    path('post', postPost),
+    path('postcomment', postPostComment),
+    path('postcommentreply', postPostCommentReply),
+    path('qna', postQna),
+    path('qnaanswer', postQnaAnswer),
+    path('qnaanswercomment', postQnaAnswerComment),
+    path('qnaanswercommentreply', postQnaAnswerCommentReply),
 ]
