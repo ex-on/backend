9437381081c5236ddebc97a0af438c5864f38cf5
YooSang Choo <rogerchoo@snu.ac.kr>
2021-12-03 21:23:03 +0900

    post 함수 오류 수정

--- community/urls.py
+++ community/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from community.views import getHotBoardPreview, getPostCommentReply, getPostPreview, getQnaAnswer, getQnaAnswerCommentReply, getQnaMain, getQnaMainSolved, getQnaMainType, getQnaMainUnsolved, postPost, postPostComment, postPostCommentReply, postQna, postQnaAnswer, postQnaAnswerComment, postQnaAnswerCommentReply
+from community.views import getHotBoardPreview, getPostCommentReply, getPostPreview, getQnaAnswer, getQnaAnswerCommentReply, getQnaMain, getQnaMainSolved, getQnaMainType, getQnaMainUnsolved, getUserPostQna, postPost, postPostComment, postPostCommentReply, postQna, postQnaAnswer, postQnaAnswerComment, postQnaAnswerCommentReply
 
 urlpatterns = [
     ########게시판 메인화면###########
@@ -10,10 +10,11 @@ urlpatterns = [
     path('qnamainsolved', getQnaMainSolved), #page_num
     path('qnamainunsolved', getQnaMainUnsolved), #page_num
     path('qnamaintype', getQnaMainType), #page_num & type
+    path('userpost', getUserPostQna),
     ########게시물 확인###############
     path('getpost', getPostCommentReply), #post_id
-    path('qnaanswer', getQnaAnswer), #qna_id
-    path('qnaanswercommentreply', getQnaAnswerCommentReply), #qna_answer_id   
+    path('getqnaanswer', getQnaAnswer), #qna_id
+    path('getqnaanswercommentreply', getQnaAnswerCommentReply), #qna_answer_id   
     ########게시물 작성###############
     path('post', postPost),
     path('postcomment', postPostComment),
