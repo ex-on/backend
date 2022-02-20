97ae932e6e19fc04612c3245ab944277f44f2ad7
정순호 <aschung@jeongsunhoui-MacBookAir.local>
2022-02-20 18:45:29 +0900

    - Updated cardio related models: added calorie record - Added user rank (protein, cardio, weight) GET API

--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import checkAvailableEmail, checkAvailableUsername, checkUserInfo, getProfileStats, getUserInfo, registerCognitoUserInfo, cognitoUserPhysicalInfo
+from .views import checkAvailableEmail, checkAvailableUsername, checkUserInfo, profileStats, getUserInfo, rankCardio, rankProtein, rankWeight, registerCognitoUserInfo, cognitoUserPhysicalInfo
 
 urlpatterns = [
     path("check_email", checkAvailableEmail),  # email
@@ -8,5 +8,8 @@ urlpatterns = [
     path("physical_info", cognitoUserPhysicalInfo),
     path("check_user_info", checkUserInfo),
     path("get_user_info", getUserInfo),
-    path('stats', getProfileStats),
+    path('stats', profileStats),
+    path('rank/protein', rankProtein),
+    path('rank/cardio', rankCardio),
+    path('rank/weight', rankWeight),
 ]
